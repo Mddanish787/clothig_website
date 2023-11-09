e37e0d1ce9a1461346e1bcc50b44d0e12748e65a
Md Danish <Mddanishbxr22@gmail.com>
2023-11-09 22:47:08 +0530

    clothing website

---
+++ index.js
@@ -0,0 +1,8 @@
+// let a = Number(prompt("Type a Number"));
+// console.log(a);
+
+// if (a === 10) {
+//   console.log("take icecream");
+// } else if (a === 30) {
+//   console.log("take coldrink");
+// }
